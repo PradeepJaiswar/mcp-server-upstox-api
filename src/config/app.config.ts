6ca14e7112f0df764b8ce13fc77e7b0b8e1d22e2
Pradeep Jaiswar <pradeepjaiswar@ip-10-10-44-169.ap-south-1.compute.internal>
2025-04-02 16:27:34 +0530

    move base url to constant from env file

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,7 +5,7 @@ export const config = {
   version: '1.0.0',
   environment: process.env.NODE_ENV || ENVIRONMENTS.DEVELOPMENT,
   upstox: {
-    apiUrl: process.env.UPSTOX_API_URL || UPSTOX_API.DEFAULT_URL,
+    apiUrl: UPSTOX_API.DEFAULT_URL,
     accessToken: process.env.UPSTOX_ACCESS_TOKEN || '',
   }
 };
